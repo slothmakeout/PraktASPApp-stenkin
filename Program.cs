04dee8c9c90747168fb7f0fe27516aa3d2c0a9cd
Daniil Stenkin <stenkin.d.s@gmail.com>
2024-05-22 17:23:41 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Builder;
+using PraktASPApp.Database;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseAuthorization();
+
+app.MapGet("/hi", () => "Hello!");
+
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
+
+app.Run();
